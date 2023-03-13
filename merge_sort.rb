b3303fcb7d1123d0bc7aa001cbf3b5e2feff3507
DNML31 <danielchun31@gmail.com>
2023-03-13 10:03:34 -0700

    Simpler pseudocode
    This commit removes the previous pseudocode and replaces it with a more
    general one. Trying to code the previous version got too complicated
    very fast so I am trying to start back from the beginning.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,33 +1,27 @@
 def merge_sort(array)
 
-  if array.length > 1
-    array_A = array[0..(array.length/2)]              # first half
-    array_B = array[(array.length/2)..array.length]   # second half
+  if array.length < 2
+    return
 
+  else 
+    a = array[0..((array.length/2)-1)]
+    # first half
     # recursive here?
 
-  else # if n is 1
-    merged = []
-    if array_A < array_B
-      merged << array_A
-    else
-      merged << array_B
+    b = array[(array.length/2)..array.length]
+    # second half
+    # recursive here?
+    
+  end
 
 
-
-  # if 'array' (or 'array_A' / 'array_B') has 1 element (base case)
-    # SELECT the two halves of 'array' for merge
-    # SELECT the first item in each array; smaller one gets MERGE/SORT in - 
-    # when one half's list is empty, add the rest of the other half to the MERGE/SORT
-  # else ('array' has > 1 elements) keep dividing each array's parts (recursive case)
-    # 'array' becomes 'array_A' and 'array_B'
 end
+# on input of n elements
+#   if n < 2
+#     return
+#   else
+#     sort left half of elements
+#     sort right half of elements 
+#     merge sorted halves
 
-array = [5,6,8,1,7,4,2,3]
-
-merge_sort(array)
-
-# divide and conquer method - 
-  #sort left side
-  #sort right side
-  #merge two sides
+merge_sort([2,1])
