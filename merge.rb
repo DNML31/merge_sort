f6753e051c5fc79314431e12362d25fbe704807e
DNML31 <danielchun31@gmail.com>
2023-03-14 13:23:29 -0700

    Change while loop condition
    This commit changed the while loop condition that handles the comparison
    between two elements in arrays 'a' and 'b'. In the previous version, I
    kept getting an error due to a comparison with a nil value. After
    changing "while (i <= x && j <= y) do" to "until (i == x || j == y) do"
    the errors weren't being raised.
    
    There is also an if..else block added after the previous block to handle
    elements that are left over once one array reaches its end. This block
    isn't correct but I wrote it in to see how it carries out the rest of
    the function. Array 'c' prints out:
    
      [nil, 2, 4, 5, nil, 6]
    
    There is still more corrections to be made but getting the correct
    number of elements seemed like a good sign.

--- merge.rb
+++ merge.rb
@@ -7,9 +7,9 @@ def merge(a, b)
 
   c = []
 
-  while (i <= x && j <= y) do
+  until (i == x || j == y) do
  
-    if (a[i] < b[0])      # b is nil?
+    if (a[i] < b[j])      # b[0] => [nil,2,4,5,6,nil,nil]
       c[k+=1] = a[i+=1]
     else
       c[k+=1] = b[j+=1]
@@ -17,9 +17,14 @@ def merge(a, b)
 
   end
 
+  if i == x
+    c[k+=1] = b[j+=1]
+  elsif j == y
+    c[k+=1] = a[i+=1]
+  end
   # one of the arrays have reached the end of their lists at this point
   # copy the remaining numbers in the other list to c.
-  # use a 'for' loop to handle array a and another loop for array b.
+  # use a 'for' loop to handle array 'a' and another loop for array 'b'.
   print c
 end
 
