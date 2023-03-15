59321c31bd16ea1d7867cf24f11b0013c3040c9a
DNML31 <danielchun31@gmail.com>
2023-03-15 09:53:46 -0700

    While loop, '<=' changed to '<'
    This commit changes the comparison operator from '<=' to '<'. Since the
    length of an array is larger than the last index of an array by 1, I
    wanted a way to exit the loop when those numbers were the same. This
    would mean that one of the arrays would be at its end and the code can
    move to the next block.
    
    In this version I commented out the block handling the remaining element(s)
    and wrote another one that checks if the next value is nil. If one array
    has a nil value, the other array's value gets sent to array 'c'. I still
    need to code a way to select the rest of the values in an array rather
    than just where the index is at.

--- merge.rb
+++ merge.rb
@@ -7,8 +7,7 @@ def merge(a, b)
 
   c = []
 
-  while (i <= x && j <= y) do
-
+  while (i < x && j < y) do
     if (a[i] < b[j])
       c[k] = a[i]
       k += 1
@@ -18,25 +17,28 @@ def merge(a, b)
       k += 1
       j += 1
     end
-    print "\nthis is c #{c}"
   end
 
-  if i <= x
-    c[k] = a[i]
-    k += 1
-    i += 1
-  elsif j <= y
-    c[k] = b[j]
-    k += 1
-    j += 1
+  # if i <= x
+  #   c[k] = a[i]
+  #   k += 1
+  #   i += 1
+  # elsif j <= y
+  #   c[k] = b[j]
+  #   k += 1
+  #   j += 1
+  # end
+
+  if a[i].nil?
+    c.insert(k, b[j])
+  elsif b[j].nil?
+    c.insert(k, a[i])
   end
-  # one of the arrays have reached the end of their lists at this point
-  # copy the remaining numbers in the other list to c.
-  # use a 'for' loop to handle array 'a' and another loop for array 'b'.
+
   print c
 end
 
-a = [2,4]
-b = [1,3]
+a = [3,5,6]
+b = [1,2,4]
 
 merge(a, b)
